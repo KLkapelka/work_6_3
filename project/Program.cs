8503e862aac25dc70628af398353abeb49c42812
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 11:45:12 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,21 @@
+﻿// Дано число. Определите будет ли это число четным и кратным 10
+
+using System;
+
+class work_6_3
+{
+    static void Main()
+    {
+        Console.Write("Введите число: "); 
+        float n = float.Parse(Console.ReadLine()); 
+
+        if (n % 2 == 0 & n % 10 == 0)
+        {
+            Console.WriteLine($"Число {n} сотвецтвует условиям");  
+        }
+        else
+        {
+            Console.WriteLine($"Число {n} не сотвецтвует условиям"); 
+        }
+    }
+}
